320d88291e87c4f5e62b18f7879f4ca44c269235
Kijoon <igreen0485@gmail.com>
2021-11-02 03:27:45 +0900

    add firebase.js to .gitignore

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from '@firebase/app';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0gBx7wsHSs0i2mNfFuOzimTNMvBrOzko",
+  authDomain: "abang-803aa.firebaseapp.com",
+  projectId: "abang-803aa",
+  storageBucket: "abang-803aa.appspot.com",
+  messagingSenderId: "308566395170",
+  appId: "1:308566395170:web:4b4dfb498510aeb98dfd7b"
+};
+
+const app = initializeApp(firebaseConfig);
