478452bb2283270d35659bdc5b55aeb97e82b0d3
이정아 <je0nga@kaist.ac.kr>
2021-11-24 21:31:15 +0900

    message

--- src/Components/Main/PopularActivity.js
+++ src/Components/Main/PopularActivity.js
@@ -3,14 +3,32 @@ import React from "react";
 import "./ShortList.css";
 
 import profileIcon from "../../Icons/Profile.png";
+import shareIcon from "../../Icons/Share.png";
+import shoppingCartIcon from "../../Icons/ShoppingCart.png";
+import hitIcon from "../../Icons/Hit_icon.png";
+import zIndex from "@material-ui/core/styles/zIndex";
 
 const ActivityItem = ({ text, hashtags }) => {
+    const resize = {
+        height : '40px',
+        width : '50px',
+        
+    }
+    const locate = {
+        zIndex:'1',
+        'margin-left' : '630px', 
+        'margin-top' : '-50px'
+    }
     return (
-        <div className="shortlist-item" style={{ backgroundColor: "var(--lightgray)" }}>
-            <img src={profileIcon} />
-            <div className="shortlist-text">
-                { text }<br/>{ hashtags }
-            </div>
+        <div className="shortlist-item" style={{ backgroundColor: "var(--lightgray)"}}>
+            <img src={hitIcon} style={{height:'50px', width:'70px'}}/>
+            <div className="shortlist-text-big">{ text }</div>
+            <div className="shortlist-text">{ hashtags }</div>   
+            <div style ={locate}>
+            <img src={shareIcon} style={resize}/> 
+            <img src={shoppingCartIcon} style={resize} /> 
+            <div line-height="130%"><br/></div>
+            </div> 
         </div>
     );
 };
@@ -22,6 +40,9 @@ const PopularActivity = () => {
         <ActivityItem text="오징어 게임 보기" hashtags="#456 #오징어"/>
         <ActivityItem text="Hey Mama 커버 댄스 찍기" hashtags="#스우파 #멋짐"/>
         <ActivityItem text="스포츠몬스터 뿌시기" hashtags="#인싸"/>
+        <ActivityItem text="파티룸에서 연말파티" hashtags="#연말 #양주 #신남"/>
+        <ActivityItem text="투다리 종강파티" hashtags="#달려 #볼링핀 #깡소주"/>
+        <ActivityItem text="교복입고 롯데월드" hashtags="#회전목마 #교복"/>
     </div>
   );
 };
--- src/Components/Main/SimilarGroup.js
+++ src/Components/Main/SimilarGroup.js
@@ -2,27 +2,40 @@ import React from "react";
 
 import "./ShortList.css";
 
-import profileIcon from "../../Icons/Profile.png";
+//import profileIcon from "../../Icons/Profile.png";
+import groupImg from "../../Icons/Default_Image.png"
+import groupNameBox from "../../Icons/groupNameBox.png"
 
 const GroupItem = ({name, hashtags}) => {
     return (
-        <div className="shortlist-item">
-            <img src={profileIcon} />
-            <div className="shortlist-text">
-                {name}<br/>{hashtags}
-            </div>
+        <div className="shortlist-item-profile">
+            <img src={groupImg} height="134px"/>
+            <center><img src={groupNameBox} className="shortList-groupname" height="40px" width='300px'/></center>
+            <center>
+                <div className="shortlist-text-groupname">{name}</div>
+                <br/>
+                <div className="shortlist-text-grouphash">{hashtags}</div>
+            </center>
         </div>
     );
 };
 
 const SimilarGroup = () => {
   return (
-    <div className="shortlist-container">
+    <div>
+    <div className="shortlist-container-small">
         <div className="shortlist-name">Similar Group</div>
         <GroupItem name="시끄러운 친구들" hashtags="#활동적 #액티비티"/>
         <GroupItem name="야행성 부엉이들" hashtags="#심야시간 #밤샘"/>
         <GroupItem name="코딩하는 대학생" hashtags="#코딩쫌재밌어"/>
     </div>
+    <div className="shortlist-container-small">
+    <div className="shortlist-name">Similar Group</div>
+    <GroupItem name="시끄러운 친구들" hashtags="#활동적 #액티비티"/>
+    <GroupItem name="야행성 부엉이들" hashtags="#심야시간 #밤샘"/>
+    <GroupItem name="코딩하는 대학생" hashtags="#코딩쫌재밌어"/>
+</div>
+</div>
   );
 };
 
