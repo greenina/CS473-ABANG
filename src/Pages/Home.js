e35ccc72cd9724cf22c1ee0b8c101bd05fcbfb4f
Kijoon <igreen0485@gmail.com>
2021-12-04 19:13:36 +0900

    add user to group collection when sign up

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,11 +4,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import {auth,  SignOut} from '../firebase'
+import {auth, db, SignOut} from '../firebase'
 import SignIn from '../Components/SignIn'
 import Main from './Main'
 import {connect} from 'react-redux'
 import {setEmail} from '../store/modules/counter'
+import { arrayUnion, updateDoc } from "firebase/firestore";
 
 const mapStateToProps = (state) => ({
   email: state.counter.email
@@ -22,15 +23,14 @@ const Home = (props) => {
   const {email, setEmail} = props
   useEffect(()=>{
     async function save(){
-      console.log("EMAIL",user.email)
-      await setEmail(user.email)
-      await console.log("email!",props.email)
+      await updateDoc(db.collection('group').doc('groupB'), {friends: arrayUnion({email:user.email, name:user.displayName})})
       await (()=>window.location.href="/main")
     }
     if(user){
-      console.log("EMAIL",user.email)
-      setEmail(user.email)
-      window.location.href="/main"
+      // console.log("EMAIL",user.email)
+      // setEmail(user.email)
+      // window.location.href="/main"
+      save()
     }
   },user)
 
